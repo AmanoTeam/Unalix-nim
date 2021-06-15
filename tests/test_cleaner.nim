f1e278ce2330900b912450dd7760803f592922a9
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-06-15 12:57:06 -0300

    Add tests

---
+++ tests/test_cleaner.nim
@@ -0,0 +1,53 @@
+import ../src/unalix
+
+var unmodifiedUrl: string
+
+unmodifiedUrl = "https://deezer.com/track/891177062?utm_source=deezer"
+
+doAssert clearUrl(unmodifiedUrl) == "https://deezer.com/track/891177062"
+doAssert clearUrl(unmodifiedUrl, ignoreRules = true) == unmodifiedUrl
+
+unmodifiedUrl = "https://www.google.com/url?q=https://pypi.org/project/Unalix"
+
+doAssert clearUrl(unmodifiedUrl) == "https://pypi.org/project/Unalix"
+doAssert clearUrl(unmodifiedUrl, ignoreRedirections = true) == unmodifiedUrl
+
+unmodifiedUrl = "https://www.google.com/amp/s/de.statista.com/infografik/amp/22496/anzahl-der-gesamten-positiven-corona-tests-und-positivenrate/"
+doAssert clearUrl(unmodifiedUrl) == "http://de.statista.com/infografik/amp/22496/anzahl-der-gesamten-positiven-corona-tests-und-positivenrate/"
+
+unmodifiedUrl = "http://www.shareasale.com/r.cfm?u=1384175&b=866986&m=65886&afftrack=&urllink=www.rightstufanime.com%2Fsearch%3Fkeywords%3DTo%20Your%20Eternity"
+doAssert clearUrl(unmodifiedUrl) == "http://www.rightstufanime.com/search?keywords=To%20Your%20Eternity"
+
+unmodifiedUrl = "https://www.amazon.com/gp/B08CH7RHDP/ref=as_li_ss_tl"
+
+doAssert clearUrl(unmodifiedUrl) == "https://www.amazon.com/gp/B08CH7RHDP"
+doAssert clearUrl(unmodifiedUrl, ignoreRawRules = true) == unmodifiedUrl
+
+unmodifiedUrl = "https://natura.com.br/p/2458?consultoria=promotop"
+
+doAssert clearUrl(unmodifiedUrl) == "https://natura.com.br/p/2458"
+doAssert clearUrl(unmodifiedUrl, ignoreReferralMarketing = true) == unmodifiedUrl
+
+unmodifiedUrl = "https://myaccount.google.com/?utm_source=google"
+
+doAssert clearUrl(unmodifiedUrl) == unmodifiedUrl
+doAssert clearUrl(unmodifiedUrl, ignoreExceptions = true) == "https://myaccount.google.com/"
+
+unmodifiedUrl = "http://clickserve.dartsearch.net/link/click?ds_dest_url=http://g.co/"
+
+doAssert clearUrl(unmodifiedUrl) == "http://g.co/"
+doAssert clearUrl(unmodifiedUrl, skipBlocked = true) == unmodifiedUrl
+
+unmodifiedUrl = "http://example.com/?p1=&p2="
+
+doAssert clearUrl(unmodifiedUrl) == unmodifiedUrl
+doAssert clearUrl(unmodifiedUrl, stripEmpty = true) == "http://example.com/"
+
+unmodifiedUrl = "http://example.com/?p1=value&p1=othervalue"
+
+doAssert clearUrl(unmodifiedUrl) == unmodifiedUrl
+doAssert clearUrl(unmodifiedUrl, stripDuplicates = true) == "http://example.com/?p1=value"
+
+unmodifiedUrl = "http://example.com/?&&&&"
+
+doAssert clearUrl(unmodifiedUrl) == "http://example.com/"
--- unalix.nimble
+++ unalix.nimble
@@ -5,10 +5,12 @@ author = "SnwMds"
 description = "Small, dependency-free, fast Nim package for removing tracking fields from URLs."
 license = "LGPL-3.0"
 srcDir = "src"
-# bin = @["unalix"]
 namedBin["unalixpkg/main"] = "unalix"
 installExt = @["nim"]
 
 # Dependencies
 
 requires "nim >= 1.2.6"
+
+task test, "Runs the test suite":
+  exec "nim compile --run tests/test_cleaner.nim"
