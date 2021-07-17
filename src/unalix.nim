b06529fe7f088b9374cd8ce4b33af0f0580b94eb
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-07-17 16:20:44 -0300

    Add missing exports

--- src/unalix.nim
+++ src/unalix.nim
@@ -1,4 +1,12 @@
 import ./unalixpkg/core
+import ./unalixpkg/exceptions
 
 export clearUrl
 export unshortUrl
+export asyncUnshortUrl
+
+export UnsupportedProtocolError
+export ReadError
+export ResolverError
+export TooManyRedirectsError
+export ConnectError
